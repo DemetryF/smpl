0049a6c1a20c8093519dc6a4f5549167f996376a
dmitry <djimabebeshko@gmail.com>
2023-01-16 18:01:15 +0300

    some collectors splits

--- src/parser/collectors/return_collector.rs
+++ src/parser/collectors/return_collector.rs
@@ -1,6 +1,9 @@
 use crate::{
     lexer::token::token_value::TokenValue,
-    parser::{ast::Statement, token_stream::TokenStream},
+    parser::{
+        ast::{Expr, Statement},
+        token_stream::TokenStream,
+    },
 };
 
 use super::expr_collector::ExprCollector;
@@ -10,14 +13,18 @@ impl ReturnStatementCollector {
     pub fn collect<'code>(token_stream: &mut TokenStream<'code>) -> Statement<'code> {
         token_stream.accept(&TokenValue::Return);
 
-        let expr = if token_stream.check(&TokenValue::Semicolon) {
-            None
-        } else {
-            Some(ExprCollector::collect(token_stream))
-        };
+        let expr = Self::return_expr(token_stream);
 
         token_stream.accept(&TokenValue::Semicolon);
 
         Statement::Return(expr)
+    }
+
+    pub fn return_expr<'code>(token_stream: &mut TokenStream<'code>) -> Option<Expr<'code>> {
+        if token_stream.check(&TokenValue::Semicolon) {
+            None
+        } else {
+            Some(ExprCollector::collect(token_stream))
+        }
     }
 }
