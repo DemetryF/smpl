23b824d3a5a5ba97b9b2942df56f03b308c5c246
Dmitry Kalinin <demetryf@yandex.ru>
2023-01-17 22:24:10 +0300

    delete folder parser/collectors

--- src/parser/collectors/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod block_collector;
-pub mod declare_collector;
-pub mod expr_collector;
-pub mod function_collector;
-pub mod if_collector;
-pub mod return_collector;
-pub mod statement_collector;
-pub mod while_collector;
