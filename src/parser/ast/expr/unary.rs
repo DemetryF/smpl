bd29235cdae4892c7c6d66898b8ccfaa2a386ade
Dmitry Kalinin <demetryf@yandex.ru>
2023-01-20 22:56:36 +0300

    make function PowerBindings::prefix return u8

--- src/parser/ast/expr/unary.rs
+++ src/parser/ast/expr/unary.rs
@@ -19,7 +19,7 @@ pub struct Unary {
 impl Collect for Unary {
     fn collect(token_stream: &mut TokenStream) -> Self {
         let op = ParserUtils::op(token_stream);
-        let ((), r_bp) = PowerBinding::prefix(op);
+        let r_bp = PowerBinding::prefix(op);
         let rhs = Expr::expr_bp(token_stream, r_bp);
 
         Self::new(op, Box::new(rhs))
