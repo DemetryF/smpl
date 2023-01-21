b69b1f9d248b5f69cb76ff7f0179635c451e16de
Dmitry Kalinin <demetryf@yandex.ru>
2023-01-21 16:33:36 +0300

    fix expr parser

--- src/parser/parser_utils.rs
+++ src/parser/parser_utils.rs
@@ -5,17 +5,14 @@ use super::{ast::expr::Expr, ast::Collect, token_stream::TokenStream};
 pub struct ParserUtils;
 impl ParserUtils {
     pub fn id(token_stream: &mut TokenStream) -> String {
-        match token_stream.current().value.clone() {
-            TokenValue::Id(value) => {
-                token_stream.skip();
-                value
-            }
+        match token_stream.skip().value.clone() {
+            TokenValue::Id(value) => value,
             _ => panic!("expected id"),
         }
     }
 
     pub fn op(token_stream: &mut TokenStream) -> Operator {
-        match token_stream.current().value.clone() {
+        match token_stream.skip().value.clone() {
             TokenValue::Operator(op) => op,
             _ => panic!("expected operator"),
         }
