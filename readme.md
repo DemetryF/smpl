dfbf88ebcaadbe97b3861e20386ba7b9fdb6e31d
Dmitry Kalinin <demetryf@yandex.ru>
2023-01-22 14:51:55 +0300

    add readme and examples

---
+++ readme.md
@@ -0,0 +1,31 @@
+# What is SMPL
+SMPL is primitive math programming language under development.
+
+# Basic Syntax
+## Declare a variable
+```cs
+define a = expr;
+```
+## While loop
+```js
+while (condition) {
+    // loop body
+}
+```
+## If Statement
+```js
+if (condition) {
+    // body
+} else {
+    // else body
+}
+```
+
+## Functions
+```js
+function name(arg1, arg2) {
+    // body
+    return expr;
+}
+```
+you can see more examples [here](https://github.com/demetryf/smpl/tree/main/examples)
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{env, fs};
 
 use crate::translator::Translator;
 
@@ -8,7 +8,9 @@ mod parser;
 mod translator;
 
 fn main() {
-    if let Ok(code) = fs::read_to_string("test.smpl") {
+    let filename = env::args().last().expect("no input file");
+
+    if let Ok(code) = fs::read_to_string(filename.clone()) {
         println!("{}", code);
 
         let mut t = Translator::new(code);
@@ -18,5 +20,7 @@ fn main() {
         for i in t.instructions {
             println!("{}", i);
         }
+    } else {
+        println!("no such file \"{}\"", filename);
     }
 }
