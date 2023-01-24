409f0602a857e050c6cac2eabb75c20b5f9a62fa
Dmitry Kalinin <demetryf@yandex.ru>
2023-01-24 21:39:52 +0300

    added bin, oct and hex number systems to lexer

--- src/lexer/token_collector/number_collector.rs
+++ src/lexer/token_collector/number_collector.rs
@@ -11,15 +11,44 @@ impl NumberCollector {
         code.current().is_ascii_digit()
     }
 
-    fn lex_number_literal(code: &mut CodeStream) -> usize {
-        let mut len = 0;
+    fn lex_num_from_rad(code: &mut CodeStream, rad: usize) -> f64 {
+        code.skip(2);
+        let buffer = Self::lex_num_literal(code, rad);
+        Self::parse_int(buffer, rad)
+    }
 
-        while !code.is_eof() && Self::is_digit(code) {
-            code.accept();
-            len += 1;
+    fn lex_dec(code: &mut CodeStream) -> f64 {
+        let mut buffer = Self::lex_num_literal(code, 10);
+
+        if code.check(".") {
+            buffer.push(code.accept());
+            buffer += Self::lex_num_literal(code, 10).as_str();
         }
 
-        len
+        buffer.parse().expect("NumberCollector")
+    }
+
+    fn parse_int(str: String, radix: usize) -> f64 {
+        let mut result = 0;
+
+        for i in 0..str.len() {
+            let c = str[i..].chars().next().expect("");
+            let n = c.to_digit(radix as u32).expect("");
+
+            result += n * radix.pow((str.len() - i - 1) as u32) as u32;
+        }
+
+        result as f64
+    }
+
+    fn lex_num_literal(code: &mut CodeStream, rad: usize) -> String {
+        let mut buffer = String::new();
+
+        while !code.is_eof() && code.current().is_digit(rad as u32) {
+            buffer.push(code.accept());
+        }
+
+        buffer
     }
 }
 
@@ -30,15 +59,12 @@ impl TokenCollector for NumberCollector {
         }
 
         let start = code.pos.index;
-        let mut len = Self::lex_number_literal(code);
-
-        if code.check(".") {
-            code.accept();
-            len += 1 + Self::lex_number_literal(code);
-        }
-
-        let source = code.get_code_slice(start, len);
-        let number = source.parse().expect("NumberCollector");
+        let number = match code.get_code_slice(start, 2) {
+            "0b" => Self::lex_num_from_rad(code, 2),
+            "0o" => Self::lex_num_from_rad(code, 8),
+            "0x" => Self::lex_num_from_rad(code, 16),
+            _ => Self::lex_dec(code),
+        };
 
         Some(TokenValue::Literal(Literal::Number(number)))
     }
