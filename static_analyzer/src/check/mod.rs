4e0a3593d57cd2effa77354efefc033798f2b210
Dmitry Kalinin <demetryf@yandex.ru>
2023-03-26 03:08:23 +0300

    remove all files

--- static_analyzer/src/check/mod.rs
+++
@@ -1,9 +0,0 @@
-use super::{env::*, StaticAnalyzer};
-
-pub mod block;
-pub mod expr;
-pub mod statement;
-
-pub trait Check {
-    fn check(&self, analyzer: &mut StaticAnalyzer, env: SharedEnv);
-}
