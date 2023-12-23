78efb8e8bdb8e80ea61fe44e54fadac311f9ee80
demetryf <djimabebeshko@gmail.com>
2023-12-23 13:31:20 +0300

    rename `SemCheck::semcheck` to `SemCheck::check`

--- semantic/smplc_semcheck/src/lib.rs
+++ semantic/smplc_semcheck/src/lib.rs
@@ -8,5 +8,5 @@ mod env;
 pub trait SemCheck: Sized {
     type Checked;
 
-    fn semcheck<'source>(self, env: Env<'source>) -> SemResult<'source, Self::Checked>;
+    fn check<'source>(self, env: Env<'source>) -> SemResult<'source, Self::Checked>;
 }
