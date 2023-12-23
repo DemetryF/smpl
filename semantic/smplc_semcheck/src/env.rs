6cc1abbb5970b81d61f012b4cec2b2e33bb7a0fe
Dmitry Kalinin <demetryf@yandex.ru>
2023-12-23 13:15:16 +0300

    write `SemCheck` trait and `Env` struct

---
+++ semantic/smplc_semcheck/src/env.rs
@@ -0,0 +1,138 @@
+use std::{collections::HashMap, rc::Rc};
+
+use smplc_hir::{FunData, FunRef, VarData, VarRef};
+
+use crate::error::{SemError, SemResult};
+
+pub struct Env<'source> {
+    pub variables: Variables<'source>,
+    pub functions: Functions<'source>,
+}
+
+pub struct Variables<'source> {
+    data: Vec<Scope<'source, VarRef>>,
+}
+
+pub struct Functions<'source> {
+    data: Scope<'source, FunRef>,
+}
+
+pub struct Scope<'source, V: Clone> {
+    data: HashMap<&'source str, V>,
+}
+
+impl<'source> Variables<'source> {
+    pub fn fork(&mut self) {
+        self.data.push(Scope::default());
+    }
+
+    pub fn exit(&mut self) {
+        self.data.pop();
+    }
+
+    fn last(&self) -> &Scope<'source, VarRef> {
+        self.data.last().unwrap()
+    }
+
+    fn last_mut(&mut self) -> &mut Scope<'source, VarRef> {
+        self.data.last_mut().unwrap()
+    }
+
+    pub fn get(&self, id: smplc_ast::Id<'source>) -> SemResult<'source, VarRef> {
+        self.last()
+            .get(id.id)
+            .ok_or_else(|| SemError::non_existent_variable(id))
+    }
+
+    pub fn add_variable(&mut self, id: smplc_ast::Id<'source>) -> SemResult<'source, VarRef> {
+        if let Some(var_ref) = self.last().get(id.id) {
+            Err(SemError::redeclaring_variable(id, var_ref.declared_at))
+        } else {
+            let smplc_ast::Id {
+                id,
+                pos: declared_at,
+            } = id;
+
+            let var_ref = Rc::new(VarData {
+                declared_at,
+                name: id.to_owned(),
+            });
+
+            self.last_mut().add(id, Rc::clone(&var_ref));
+
+            Ok(var_ref)
+        }
+    }
+
+    pub fn add_argument(&mut self, id: smplc_ast::Id<'source>) -> SemResult<'source, VarRef> {
+        if self.last().has(id.id) {
+            Err(SemError::duplicate_args_names(id))
+        } else {
+            let smplc_ast::Id {
+                id,
+                pos: declared_at,
+            } = id;
+
+            let var_ref = Rc::new(VarData {
+                declared_at,
+                name: id.to_owned(),
+            });
+
+            self.last_mut().add(id, Rc::clone(&var_ref));
+
+            Ok(var_ref)
+        }
+    }
+}
+
+impl<'source> Functions<'source> {
+    pub fn get(&self, id: smplc_ast::Id<'source>) -> SemResult<'source, FunRef> {
+        self.data
+            .get(id.id)
+            .ok_or_else(|| SemError::non_existent_function(id))
+    }
+
+    pub fn add(
+        &mut self,
+        id: smplc_ast::Id<'source>,
+        args_count: usize,
+    ) -> SemResult<'source, FunRef> {
+        if let Some(fun_ref) = self.data.get(id.id) {
+            Err(SemError::redeclaring_function(id, fun_ref.declared_at))
+        } else {
+            let smplc_ast::Id { id, pos } = id;
+
+            let fun_ref = Rc::new(FunData {
+                declared_at: pos,
+                name: id.to_owned(),
+                args_count,
+            });
+
+            self.data.add(id, Rc::clone(&fun_ref));
+
+            Ok(fun_ref)
+        }
+    }
+}
+
+impl<'source, V: Clone> Scope<'source, V> {
+    pub fn get(&self, id: &'source str) -> Option<V> {
+        self.data.get(id).cloned()
+    }
+
+    pub fn add(&mut self, id: &'source str, value: V) {
+        self.data.insert(id, value);
+    }
+
+    pub fn has(&self, id: &'source str) -> bool {
+        self.data.contains_key(id)
+    }
+}
+
+impl<V: Clone> Default for Scope<'_, V> {
+    fn default() -> Self {
+        Self {
+            data: Default::default(),
+        }
+    }
+}
--- semantic/smplc_semcheck/src/error.rs
+++ semantic/smplc_semcheck/src/error.rs
@@ -1,4 +1,5 @@
 use smplc_ast::Pos;
+use smplc_hir::FunRef;
 
 pub type SemResult<'source, T> = Result<T, SemError<'source>>;
 
@@ -16,7 +17,7 @@ pub enum SemErrorKind<'source> {
         first_declaration: Pos,
     },
 
-    ReDeclaringFunction {
+    RedeclaringFunction {
         id: &'source str,
         first_declaration: Pos,
     },
@@ -24,8 +25,74 @@ pub enum SemErrorKind<'source> {
     InvalidArguments {
         expected_args_count: usize,
         received_args_count: usize,
-        function_id: &'source str,
+        function_ref: FunRef,
     },
 
     DuplicateArgsNames(&'source str),
+}
+
+impl<'source> SemError<'source> {
+    pub fn non_existent_variable(id: smplc_ast::Id<'source>) -> Self {
+        let smplc_ast::Id { id, pos } = id;
+
+        Self {
+            kind: SemErrorKind::NonExistentVariable(id),
+            pos,
+        }
+    }
+
+    pub fn non_existent_function(id: smplc_ast::Id<'source>) -> Self {
+        let smplc_ast::Id { id, pos } = id;
+
+        Self {
+            kind: SemErrorKind::NonExistentFunction(id),
+            pos,
+        }
+    }
+
+    pub fn redeclaring_variable(id: smplc_ast::Id<'source>, first_declaration: Pos) -> Self {
+        let smplc_ast::Id { id, pos } = id;
+
+        let kind = SemErrorKind::RedeclaringVariable {
+            id,
+            first_declaration,
+        };
+
+        Self { kind, pos }
+    }
+
+    pub fn redeclaring_function(id: smplc_ast::Id<'source>, first_declaration: Pos) -> Self {
+        let smplc_ast::Id { id, pos } = id;
+
+        let kind = SemErrorKind::RedeclaringFunction {
+            id,
+            first_declaration,
+        };
+
+        Self { kind, pos }
+    }
+
+    pub fn invalid_arguments(
+        pos: Pos,
+        expected_args_count: usize,
+        received_args_count: usize,
+        function_ref: FunRef,
+    ) -> Self {
+        let kind = SemErrorKind::InvalidArguments {
+            expected_args_count,
+            received_args_count,
+            function_ref,
+        };
+
+        Self { kind, pos }
+    }
+
+    pub fn duplicate_args_names(id: smplc_ast::Id<'source>) -> Self {
+        let smplc_ast::Id { id, pos } = id;
+
+        Self {
+            kind: SemErrorKind::DuplicateArgsNames(id),
+            pos,
+        }
+    }
 }
