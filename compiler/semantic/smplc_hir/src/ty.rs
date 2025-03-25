855bf653527c2f345f410882b50e9b6b6f27c6b2
Dmitry Kalinin <demetryf@proton.me>
2025-03-25 22:01:09 +0300

    semantic: add `Vec2`, `Vec3`, `Vec4` and `Complex` variants to `Type`

--- compiler/semantic/smplc_hir/src/ty.rs
+++ compiler/semantic/smplc_hir/src/ty.rs
@@ -7,6 +7,10 @@ pub enum Type {
     Real,
     Int,
     Bool,
+    Vec2,
+    Vec3,
+    Vec4,
+    Complex,
 }
 
 impl TryFrom<&str> for Type {
@@ -17,6 +21,10 @@ impl TryFrom<&str> for Type {
             "real" => Ok(Self::Real),
             "int" => Ok(Self::Int),
             "bool" => Ok(Self::Bool),
+            "vec2" => Ok(Self::Vec2),
+            "vec3" => Ok(Self::Vec3),
+            "vec4" => Ok(Self::Vec4),
+            "complex" => Ok(Self::Complex),
 
             _ => Err(()),
         }
@@ -39,6 +47,10 @@ impl fmt::Display for Type {
             Type::Real => write!(f, "real"),
             Type::Int => write!(f, "int"),
             Type::Bool => write!(f, "bool"),
+            Type::Vec2 => write!(f, "vec2"),
+            Type::Vec3 => write!(f, "vec3"),
+            Type::Vec4 => write!(f, "vec4"),
+            Type::Complex => write!(f, "complex"),
         }
     }
 }
